add09e9edd78f1eb9fce7767f4f463b853a16ae1
Nik <nikitos08.1@mail.ru>
2022-11-28 21:50:58 +0700

    fix index components

--- src/components/index.ts
+++ src/components/index.ts
@@ -14,6 +14,7 @@ export { Header } from "./Header";
 export { Input } from "./Input";
 export { Link } from "./Link";
 export { List } from "./List";
+export { ListItem } from "./ListItem";
 export { Menu } from "./Menu";
 export { MenuItem } from "./MenuItem";
 export { MenuList } from "./MenuList";
